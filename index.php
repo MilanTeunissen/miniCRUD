a512169bbd54d004b20783fe81abb7b57e335614
BigDickMilan <milantheunissen2003@gmail.com>
2022-02-21 15:18:40 +0100

    /

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Asap&display=swap"
+      rel="stylesheet"
 </head>
 <body>
     <main>
         <?php echo("Sluipschutters is bea"); ?> 
-        <div class="blok"></div>
    </main>
 </body>
 </html>
