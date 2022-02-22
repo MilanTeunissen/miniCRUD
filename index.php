789db771ba058784e8a55376e8c4f045ccd01635
BigDickMilan <milantheunissen2003@gmail.com>
2022-02-22 13:53:00 +0100

    /

--- index.php
+++ index.php
@@ -4,13 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Asap&family=Roboto:wght@300&display=swap" rel="stylesheet">
     <title>Pizza Tent</title>
 </head>
 <body>
+    <div class="page">
+        <nav class="page__menu menu">
+          <ul class="menu__list r-list">
+            <li class="menu__group"><a href="#0" class="menu__link r-link text-underlined">Option #1</a></li>
+            <li class="menu__group"><a href="#0" class="menu__link r-link text-underlined">Option #2</a></li>
+            <li class="menu__group"><a href="#0" class="menu__link r-link text-underlined">Option #3</a></li>
+            <li class="menu__group"><a href="#0" class="menu__link r-link text-underlined">Option #4</a></li>
+            <li class="menu__group"><a href="#0" class="menu__link r-link text-underlined">Option #5</a></li>
+          </ul>
+        </nav>
     <main>
         <h1>Sluipschutter is bea</h1>
    </main>
